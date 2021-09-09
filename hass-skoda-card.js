432f4cc38bfde15721b757e7bbbd3006da5eb24a
Kim <faekie@hotmail.com>
2021-09-09 22:29:42 +0200

    ändrad:        hass-skoda-card.js

--- hass-skoda-card.js
+++ hass-skoda-card.js
@@ -7,10 +7,9 @@ import {
 class SkodaCard extends LitElement {
   static get properties() {
     return {
-      hass: {},
-      config: {},
-      imageurl: 'https://ip-modcwp.azureedge.net//modcwp3v5b20200903/0F0F-2ovtqWUlebk-6kVu2o9bdY-17NgmyXQTv.qVlH-yIC8G.6aPQ2EXMUzv-s1BZqeSWgmRyK69.kcuHUNDxO-DiSFcTLXOgdPZlAGIn-1080570studiovbeauty_connectview101281.png?v=637169955100000000'
-      //${this.hass.states['device_tracker.superb_position'].attributes.entity_picture}
+      hass: { type: Object },
+      config: { type: Object },
+      imageurl: { type: String }
     };
   }
 
@@ -38,14 +37,16 @@ class SkodaCard extends LitElement {
   }
 
   setConfig(config) {
-    if (!config.device) {
-      throw new Error('Please define a device (vehicle name)');
-    }
+    //if (!config.device) {
+    //  throw new Error('Please define a device (vehicle name)');
+    //}
     if (!config.entities) {
       throw new Error('Please define a list of entities');
     }
     this.config = config;
-  }
+    this.imageurl: 'https://ip-modcwp.azureedge.net//modcwp3v5b20200903/0F0F-2ovtqWUlebk-6kVu2o9bdY-17NgmyXQTv.qVlH-yIC8G.6aPQ2EXMUzv-s1BZqeSWgmRyK69.kcuHUNDxO-DiSFcTLXOgdPZlAGIn-1080570studiovbeauty_connectview101281.png?v=637169955100000000'
+    //${this.hass.states['device_tracker.superb_position'].attributes.entity_picture}
+}
 
   // @TODO: This requires more intelligent logic
   getCardSize() {
