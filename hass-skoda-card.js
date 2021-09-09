660097f3e691435bef1af7aa30c409497550c70a
Kim <faekie@hotmail.com>
2021-09-09 22:23:40 +0200

    ändrad:        hass-skoda-card.js

--- hass-skoda-card.js
+++ hass-skoda-card.js
@@ -9,25 +9,16 @@ class SkodaCard extends LitElement {
     return {
       hass: {},
       config: {},
-      imageurl: ''
+      imageurl: 'https://ip-modcwp.azureedge.net//modcwp3v5b20200903/0F0F-2ovtqWUlebk-6kVu2o9bdY-17NgmyXQTv.qVlH-yIC8G.6aPQ2EXMUzv-s1BZqeSWgmRyK69.kcuHUNDxO-DiSFcTLXOgdPZlAGIn-1080570studiovbeauty_connectview101281.png?v=637169955100000000'
+      //${this.hass.states['device_tracker.superb_position'].attributes.entity_picture}
     };
-  }
-
-  setConfig(config) {
-    if (!config.device) {
-      throw new Error('Please define a device (vehicle name)');
-    }
-    if (!config.entities) {
-      throw new Error('Please define a list of entities');
-    }
-    this.config = config;
   }
 
   render(){
     return html`
       <skoda-card>
         <div id="container">
-          <img id="skoda-model" src="${this.hass.states['device_tracker.superb_position'].attributes.entity_picture}" style="display:block">
+          <img id="skoda-model" src="${this.imageurl}" style="display:block">
           ${this.config.entities.map(entity => {
           const stateObj = this.hass.states[entity];
           return stateObj
@@ -44,6 +35,16 @@ class SkodaCard extends LitElement {
         </div>
         </skoda-card>
     `;
+  }
+
+  setConfig(config) {
+    if (!config.device) {
+      throw new Error('Please define a device (vehicle name)');
+    }
+    if (!config.entities) {
+      throw new Error('Please define a list of entities');
+    }
+    this.config = config;
   }
 
   // @TODO: This requires more intelligent logic
