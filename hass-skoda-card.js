0195294f604ac6d9c7445c4d187b43cb8bd9e6ad
Kim <faekie@hotmail.com>
2021-09-09 21:51:49 +0200

    ändrad:        hass-skoda-card.js

--- hass-skoda-card.js
+++ hass-skoda-card.js
@@ -1,43 +1,63 @@
-class SkodaCard extends HTMLElement {
-  set hass(hass) {
-    const device = this.config.device;
-    const tracker = 'device_tracker.'.concat(device, '_position')
-    const moving = 'binary_sensor.'.concat(device, '_vehicle_moving')
-    const tracker_state = hass.states[tracker]
-    const moving_state = hass.states[moving]
-    const image_url = tracker_state.attributes.entity_picture
+import { LitElement, html } from 'lit-element';
 
-    if (!this.content) {
-      this.innerHTML = `
-        <ha-card header="Skoda Connect">
-          <div class="card-content"></div>
-        </ha-card>
-      `;
-      this.content = this.querySelector('div');
-    }
-
-    this.content.innerHTML = `
-      The chosen vehicle ${device}, device tracker sensor is ${tracker} and moving sensor is ${moving}!
-      <br><br>
-      <img src="${image_url}">
-    `;
+class SkodaCard extends LitElement {
+  static get properties() {
+    return {
+      hass: {},
+      config: {},
+      imageurl: ''
+    };
   }
 
-  // The user supplied configuration. Throw an exception and Lovelace will
-  // render an error card.
   setConfig(config) {
     if (!config.device) {
-      throw new Error('Please define a device (car name)!');
+      throw new Error('Please define a device (vehicle name)');
+    }
+    if (!config.entities) {
+      throw new Error('Please define a list of entities');
     }
     this.config = config;
   }
 
-  // The height of your card. Home Assistant uses this to automatically
-  // distribute all cards over the available columns.
+  render(){
+    return html`
+      <div id="skoda-card">
+        <div id="container">
+          <img id="skoda-model" src="${this.imageurl}" style="display:block">
+          ${this.config.entities.map(entity => {
+          const stateObj = this.hass.states[entity];
+          return stateObj
+            ? html`
+              <div class="state">
+                ${stateObj.attributes.friendly_name}:
+                ${stateObj.state}
+              </div>
+            `
+            : html`
+              <div class="not-found">Entity ${entity} not found.</div>
+            `;
+          })}
+        </div>
+      </div>
+    `;
+  }
+
+  set hass(hass) {
+    this.hass = hass;
+    const name = this.config.device;
+
+    if (this.config.title == null || this.config.title == true) {
+      this.header = `Skoda ${name.charAt(0).toUpperCase() + name.slice(1)}`;
+    }
+
+    const base_entity = this.config.device.toLowerCase();
+    this.imageurl = this.hass.states[`device_tracker.${base_entity}_position`].attributes.entity_picture;
+  }
+
+  // @TODO: This requires more intelligent logic
   getCardSize() {
-    // 150px high
     return 3;
   }
 }
 
-customElements.define("skoda-card", SkodaCard);
+customElements.define('skoda-card', SkodaCard);
